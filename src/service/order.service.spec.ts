e4ce1eda79d0ed6f1ca352ece8a26a23292b807a
WagnerReis <wagnerreis1997@gmail.com>
2023-01-30 16:14:15 -0300

    Testando regra de reward points

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -26,4 +26,15 @@ describe("Order service unit tests", () => {
 
     expect(total).toBe(500)
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
