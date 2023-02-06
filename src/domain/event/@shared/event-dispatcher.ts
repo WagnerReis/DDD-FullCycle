dd55875dd356615c52d5702884745d910e0d3560
WagnerReis <wagnerreis1997@gmail.com>
2023-02-06 08:39:21 -0300

    Desregistrando eventos

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -10,16 +10,24 @@ export default class EventDispatcher implements EventDispatcherInterface {
   }
 
   register(eventName: string, eventHandler: EventHandlerInterface): void {
-    if(!this.eventHandlers[eventName]) {
+    if (!this.eventHandlers[eventName]) {
       this.eventHandlers[eventName] = [];
     }
     this.eventHandlers[eventName].push(eventHandler);
   }
 
   unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (this.eventHandlers[eventName]) {
+      const index = this.eventHandlers[eventName].indexOf(eventHandler);
+      if (index !== -1) {
+        this.eventHandlers[eventName].splice(index, 1);
+      }
+    }
   }
 
   notify(event: EventInterface): void {}
 
-  unregisterAll(): void {}
+  unregisterAll(): void {
+    this.eventHandlers = {};
+  }
 }
