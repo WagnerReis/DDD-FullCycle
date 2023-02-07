8295599606bc579e14e6ed6d64525f78ed595a0a
WagnerReis <wagnerreis1997@gmail.com>
2023-02-06 21:50:29 -0300

    Adicionado AddressChangedEvent

---
+++ src/domain/event/customer/address-changed-handler.spec.ts
@@ -0,0 +1,38 @@
+import Address from "../../entity/address";
+import Customer from "../../entity/customer";
+import EventDispatcher from "../@shared/event-dispatcher";
+import CustomerCreatedEvent from "./customer-created.event";
+import EnviaConsoleLogHandler from "./handle/envia-console-log.handler";
+
+describe("Address event handle tests", () => {
+  it("should call handles to change address of customer", () => {
+    const eventDispatcher = new EventDispatcher();
+
+    const customer = new Customer("123", "Jhon");
+    const address = new Address("Street 2", 2, "zipcode 2", "city 2");
+    customer.changeAddress(address);
+
+    const eventHandler1 = new EnviaConsoleLogHandler()
+
+    const spyEventHandler = jest.spyOn(eventHandler1, "handle");
+
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
+
+    const customerCreatedEvent = new CustomerCreatedEvent({
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: address.street,
+        number: address.number,
+        zip: address.zip,
+        city: address.city,
+      }
+    });
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
+  });
+});
--- src/domain/event/customer/customer-event-handle.spec.ts
+++ src/domain/event/customer/customer-event-handle.spec.ts
@@ -2,7 +2,6 @@ import Address from "../../entity/address";
 import Customer from "../../entity/customer";
 import EventDispatcher from "../@shared/event-dispatcher";
 import CustomerCreatedEvent from "./customer-created.event";
-import EnviaConsoleLogHandler from "./handle/envia-console-log.handler";
 import EnviaConsoleLog1Handler from "./handle/envia-console-log1.handler";
 import EnviaConsoleLog2Handler from "./handle/envia-console-log2.handler";
 
@@ -31,36 +30,5 @@ describe("Customer event handle tests", () => {
 
     expect(spyEventHandler1).toHaveBeenCalled();
     expect(spyEventHandler2).toHaveBeenCalled();
-  });
-
-  it("should call handles to change address of customer", () => {
-    const eventDispatcher = new EventDispatcher();
-
-    const customer = new Customer("123", "Jhon");
-    const address = new Address("Street 2", 2, "zipcode 2", "city 2");
-    customer.changeAddress(address);
-
-    const eventHandler1 = new EnviaConsoleLogHandler()
-
-    const spyEventHandler = jest.spyOn(eventHandler1, "handle");
-
-    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
-
-    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
-
-    const customerCreatedEvent = new CustomerCreatedEvent({
-      id: customer.id,
-      name: customer.name,
-      address: {
-        street: address.street,
-        number: address.number,
-        zip: address.zip,
-        city: address.city,
-      }
-    });
-
-    eventDispatcher.notify(customerCreatedEvent);
-
-    expect(spyEventHandler).toHaveBeenCalled();
   });
 });
